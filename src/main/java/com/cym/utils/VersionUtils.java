af0d4784f44db1038b87565f9fa425855dcec143
dyrnq <dyrnq@qq.com>
2023-05-31 11:25:57 +0800

    重写VersionUtils类的getVersion()方法

--- src/main/java/com/cym/utils/VersionUtils.java
+++ src/main/java/com/cym/utils/VersionUtils.java
@@ -1,37 +1,40 @@
 package com.cym.utils;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class VersionUtils {
 
-    public static String getVersion() throws FileNotFoundException, IOException {
-
-        // 查看jar包里面pom.properties版本号
-//        String jarPath = VersionUtils.class.getProtectionDomain().getCodeSource().getLocation().getFile();
-//        jarPath = java.net.URLDecoder.decode(jarPath, "UTF-8");
-//        try {
-//            URL url = new URL("jar:file:" + jarPath + "!/META-INF/maven/com.dyrnq/nginxWebUI/pom.properties");
-//            InputStream inputStream = url.openStream();
-//            Properties properties = new Properties();
-//            properties.load(inputStream);
-//            String version = properties.getProperty("version");
-//            return version;
-//        } catch (Exception e) {
-//            // 开发过程中查看pom.xml版本号
-//            MavenXpp3Reader reader = new MavenXpp3Reader();
-//            String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-//            if (SystemTool.isWindows() && basePath.startsWith("/")) {
-//                basePath = basePath.substring(1);
-//            }
-//            if (basePath.indexOf("/target/") != -1) {
-//                basePath = basePath.substring(0, basePath.indexOf("/target/"));
-//            }
-//            Model model = reader.read(new FileReader(new File(basePath, "pom.xml")));
-//            String version = model.getVersion();
-//            return version;
-//        }
-
-        return "1.0.0";
+    public static String getVersion() throws Exception {
+        // 查看jar包里面build.info版本号
+        String jarPath = VersionUtils.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+        jarPath = java.net.URLDecoder.decode(jarPath, "UTF-8");
+        try {
+            URL url = new URL("jar:file:" + jarPath + "!/build.info");
+            InputStream inputStream = url.openStream();
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            String version = properties.getProperty("project.version");
+            return version;
+        } catch (Exception e) {
+            // 开发过程中查看pom.xml版本号
+            String xmlContent = new String(Files.readAllBytes(Paths.get("pom.xml")));
+            // 使用正则表达式匹配版本号
+            Pattern pattern = Pattern.compile("<version>(.*?)<\\/version>");
+            Matcher matcher = pattern.matcher(xmlContent);
+            String version="";
+            if (matcher.find()) {
+                version = matcher.group(1);
+            }
+            return StringUtils.trim(version);
+        }
+        //return "1.0.0";
     }
 }
