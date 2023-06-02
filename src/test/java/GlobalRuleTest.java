de64f7db5880aa6319e59ffb6804329f89f3aa49
dyrnq <dyrnq@qq.com>
2023-06-02 14:25:02 +0800

    新增一个GlobalRuleTest用于测试全局规则配置

---
+++ src/test/java/GlobalRuleTest.java
@@ -0,0 +1,35 @@
+import com.apiseven.apisix.common.exception.ApisixSDKExcetion;
+import com.dyrnq.apisix.domain.GlobalRule;
+import com.dyrnq.apisix.domain.Route;
+import com.dyrnq.apisix.plugins.*;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalRuleTest extends BaseJunit{
+    @Test
+    public void test_GlobalRule() throws ApisixSDKExcetion {
+        GlobalRule globalRule = new GlobalRule();
+        java.util.Map map = new HashMap();
+        Echo e = new Echo();
+        e.headers = new HashMap<String, String>();
+        e.headers.put("hello", "world");
+        e.afterBody = "<!-- 111 apisix-HTML-mark -->";
+        map.put("echo", e);
+        ProxyRewrite proxyRewrite = new ProxyRewrite();
+        proxyRewrite.headers = new Headers();
+        proxyRewrite.headers.set = new HashMap<>();
+        proxyRewrite.headers.set.put( "X-Real-IP","$remote_addr");
+        proxyRewrite.headers.set.put( "X-Forwarded-For","$proxy_add_x_forwarded_for");
+        proxyRewrite.headers.set.put( "X-Test-1","test1");
+        proxyRewrite.headers.set.put( "X-Test-2","test2");
+        proxyRewrite.headers.set.put( "X-Test-3","test3");
+        proxyRewrite.headers.set.put( "X-Test-4","test4");
+
+        map.put("proxy-rewrite",proxyRewrite);
+        globalRule.setPlugins(map);
+        //consumer.setGroupId(i+"");
+        client.putGlobalRule("1", globalRule);
+    }
+}
