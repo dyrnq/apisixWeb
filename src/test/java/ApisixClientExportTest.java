fc00b99d5ebf4e548f5b4e80ffbb0eee21e02249
ash <1343093151@qq.com>
2023-06-09 10:37:50 +0800

    导入导出测试队列增加压缩解压功能。

--- src/test/java/ApisixClientExportTest.java
+++ src/test/java/ApisixClientExportTest.java
@@ -1,20 +1,27 @@
+import cn.hutool.core.util.ZipUtil;
 import com.dyrnq.apisix.ApisixSDKException;
 import com.dyrnq.apisix.domain.*;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.apache.commons.io.IOUtils;
+import org.codehaus.plexus.archiver.tar.TarEntry;
+import org.codehaus.plexus.archiver.tar.TarInputStream;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ApisixClientExportTest extends BaseJunit {
     static final String pathString = "C:\\Users\\ash\\Desktop\\export";
@@ -32,7 +39,7 @@ public class ApisixClientExportTest extends BaseJunit {
     @Test
     public void test_export() throws ApisixSDKException, IOException {
         Gson gson = new Gson();//创建gson对象，含有转化的toJson方法
-
+        createDir("export","C:\\Users\\ash\\Desktop");
         //1.route队列
         String folderName1 = "route";
         createDir(folderName1, pathString);
@@ -167,14 +174,104 @@ public class ApisixClientExportTest extends BaseJunit {
 
 
         }
+        //11.proto队列
+        String folderName11 = "proto";
+        createDir(folderName11, pathString);
+        List<Proto> protos = client.listProtos();
+        for (Proto obj : protos) {
+            File file = new File(pathString + "/" + folderName11 + "/" + obj.getId() + ".json");//创建file文件地址对象，作为载体
+            FileWriter writer = new FileWriter(file);//创建writer对象，含有写入方法。
+            String json = gson.toJson(obj);//创立json字符串形式对象，接收转化后的route （java对象）→（字符串）
+            writer.write(json);//执行写入方法。
+            writer.close();//关闭写入方法。
 
+
+        }
+
+
+        String sourceDirPath = "C:\\Users\\ash\\Desktop\\export";
+        String targetFilePath = "C:\\Users\\ash\\Desktop\\jar.tar.gz";
+        tarGz(sourceDirPath,targetFilePath);
     }
 
+    private static void tarGz(String sourceDirPath, String targetFilePath) throws IOException {
+
+
+        FileOutputStream fileOutputStream = new FileOutputStream(targetFilePath);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        GzipCompressorOutputStream gzipOutputStream = new GzipCompressorOutputStream(bufferedOutputStream);
+        TarArchiveOutputStream tarArchiveOutputStream = new TarArchiveOutputStream(gzipOutputStream);
+
+        File sourceDir = new File(sourceDirPath);
+        for (File file : sourceDir.listFiles()) {
+            addFileToTarGz(tarArchiveOutputStream, "", file);
+        }
+
+        tarArchiveOutputStream.close();
+        gzipOutputStream.close();
+        bufferedOutputStream.close();
+        fileOutputStream.close();
+    }
+
+    private static void addFileToTarGz(TarArchiveOutputStream tarArchiveOutputStream, String base, File file) throws IOException {
+        String entryName = base + file.getName();
+        TarArchiveEntry tarEntry = new TarArchiveEntry(file, entryName);
+        tarArchiveOutputStream.putArchiveEntry(tarEntry);
+
+        if (file.isFile()) {
+            try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
+                byte[] buffer = new byte[1024];
+                int count;
+                while ((count = inputStream.read(buffer)) != -1) {
+                    tarArchiveOutputStream.write(buffer, 0, count);
+                }
+            }
+            tarArchiveOutputStream.closeArchiveEntry();
+        } else if (file.isDirectory()) {
+            tarArchiveOutputStream.closeArchiveEntry();
+            for (File childFile : file.listFiles()) {
+                String childBase = entryName + "/";
+                addFileToTarGz(tarArchiveOutputStream, childBase, childFile);
+            }
+        }
+    }
+
+
     //测试能否将外部txt文件以对象形式导入数据库
+
+
+    public static void extract(String tarGzFilepath, String destDirectory) throws IOException {
+        FileInputStream fis = new FileInputStream(tarGzFilepath);
+        GzipCompressorInputStream gzis = new GzipCompressorInputStream(fis);
+        TarArchiveInputStream tais = new TarArchiveInputStream(gzis);
+
+        TarArchiveEntry entry;
+        while ((entry = tais.getNextTarEntry()) != null) {
+            File outputFile = new File(destDirectory, entry.getName());
+            if (entry.isDirectory()) {
+                // 如果是目录，则创建目录
+                outputFile.mkdirs();
+            } else {
+                // 如果是文件，则写入文件内容
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                FileOutputStream fos = new FileOutputStream(outputFile);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                while ((bytesRead = tais.read(buffer)) != -1) {
+                    bos.write(buffer, 0, bytesRead);
+                }
+                bos.close();
+            }
+        }
+        tais.close();
+    }
+
+
+
     @Test
     public void test_import() throws ApisixSDKException, IOException {
         Gson gson = new Gson();
-
+        extract("C:\\Users\\ash\\Desktop\\jar.tar.gz","C:\\Users\\ash\\Desktop\\export");//执行解压方法
         //1.route队列
         File folder1 = new File(pathString + "/route");
         for (File file1 : folder1.listFiles()) {
