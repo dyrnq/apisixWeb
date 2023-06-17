507bffe05441ecdcb9aa3ae0f27e12c1cdc2a3f5
dyrnq <dyrnq@qq.com>
2023-06-17 13:33:20 +0800

    将HomeDir的配置初始化移动到Config中(@Configuration注解的优先级较高)

--- src/main/java/com/dyrnq/Config.java
+++ src/main/java/com/dyrnq/Config.java
@@ -1,12 +1,22 @@
 package com.dyrnq;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RegExUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
+import org.noear.solon.annotation.Inject;
 import org.noear.solon.i18n.LocaleResolver;
 import org.noear.solon.i18n.impl.LocaleResolverCookie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 @Configuration
 public class Config {
+    static Logger logger = LoggerFactory.getLogger(Config.class);
     // typed=true，表示默认数据源。@Db 可不带名字注入
 //    @Bean(value = "db1" ,typed = true)
 //    public DataSource db1(@Inject("${test.db1}") HikariDataSource ds) throws Exception{
@@ -22,6 +32,36 @@ public class Config {
     public LocaleResolver localInit() {
         return new LocaleResolverCookie();
     }
+    @Inject("${project.home:}")
+    private String home;
 
+    @Inject("${solon.app.name}")
+    String projectName;
+
+    @Bean(value = "homeDir", typed = true)
+    HomeDir getHomeDir(){
+        String homeAbsolutePath="";
+        String tmpAbsolutePath="";
+        String systemUserDir = SystemUtils.getUserHome().getAbsolutePath();
+        if(StringUtils.isBlank(home)){
+            homeAbsolutePath=systemUserDir + File.separator+"."+projectName;
+        }else{
+            if(StringUtils.startsWith(home,"~")){
+                homeAbsolutePath= RegExUtils.replaceFirst(home,"~",systemUserDir);
+            }else{
+                homeAbsolutePath=home;
+            }
+        }
+        tmpAbsolutePath= homeAbsolutePath+File.separator+"tmp";
+
+        try{
+            FileUtils.forceMkdir(new File(tmpAbsolutePath));
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }
+        logger.info("homeAbsolutePath="+homeAbsolutePath);
+        logger.info("tmpAbsolutePath="+tmpAbsolutePath);
+        return new HomeDir(homeAbsolutePath,tmpAbsolutePath);
+    }
 
 }
--- src/main/java/com/dyrnq/HomeDir.java
+++ src/main/java/com/dyrnq/HomeDir.java
@@ -1,56 +1,27 @@
 package com.dyrnq;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.RegExUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.noear.solon.annotation.Component;
-import org.noear.solon.annotation.Inject;
-import org.noear.solon.core.bean.LifecycleBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-
-@Component
-public class HomeDir implements LifecycleBean {
+public class HomeDir {
     static Logger logger = LoggerFactory.getLogger(HomeDir.class);
-    @Inject("${project.home:}")
-    private String home;
 
-    @Inject("${solon.app.name}")
-    String projectName;
-    @Inject
-    CfgExtractor cfgExtractor;
     private String homeAbsolutePath;
     private String tmpAbsolutePath;
 
-    @Override
-    public void start() throws Throwable {
-        String systemUserDir = SystemUtils.getUserHome().getAbsolutePath();
-        if(StringUtils.isBlank(home)){
-            homeAbsolutePath=systemUserDir + File.separator+"."+projectName;
-        }else{
-            if(StringUtils.startsWith(home,"~")){
-                homeAbsolutePath= RegExUtils.replaceFirst(home,"~",systemUserDir);
-            }else{
-                homeAbsolutePath=home;
-            }
-        }
-        tmpAbsolutePath= homeAbsolutePath+File.separator+"tmp";
-
-        try{
-            FileUtils.forceMkdir(new File(tmpAbsolutePath));
-        }catch (Exception e){
-            logger.error(e.getMessage());
-        }
+    public HomeDir(){
+        super();
+    }
+    public HomeDir(String homeAbsolutePath,String tmpAbsolutePath){
+        this.homeAbsolutePath = homeAbsolutePath;
+        this.tmpAbsolutePath = tmpAbsolutePath;
     }
 
-
-    public String getHomeAbsolutePath(){
+    public String getHomeAbsolutePath() {
         return homeAbsolutePath;
     }
-    public String getTmpAbsolutePath(){
+
+    public String getTmpAbsolutePath() {
         return tmpAbsolutePath;
     }
 }
