4e013ade6c9c1618df2b6b9a90af440bd932fe54
dyrnq <dyrnq@qq.com>
2023-07-03 17:36:11 +0800

    补充Protocol类字段

---
+++ src/main/java/com/dyrnq/apisix/domain/Logger.java
@@ -0,0 +1,65 @@
+package com.dyrnq.apisix.domain;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Logger {
+
+    /**
+     * logger filter rules
+     */
+    @SerializedName("filter")
+    @Expose
+    private List<Object> filter = new ArrayList<Object>();
+    /**
+     * logger plugin configuration
+     */
+    @SerializedName("conf")
+    @Expose
+    private Map<String, Object> conf;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    /**
+     * logger filter rules
+     */
+    public List<Object> getFilter() {
+        return filter;
+    }
+
+    /**
+     * logger filter rules
+     */
+    public void setFilter(List<Object> filter) {
+        this.filter = filter;
+    }
+
+    /**
+     * logger plugin configuration
+     */
+    public Map<String, Object> getConf() {
+        return conf;
+    }
+
+    /**
+     * logger plugin configuration
+     */
+    public void setConf(Map<String, Object> conf) {
+        this.conf = conf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- src/main/java/com/dyrnq/apisix/domain/Protocol.java
+++ src/main/java/com/dyrnq/apisix/domain/Protocol.java
@@ -1,33 +1,62 @@
 package com.dyrnq.apisix.domain;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-public class Protocol{
+
+import java.util.List;
+import java.util.Map;
+
+public class Protocol {
     @SerializedName("name")
     @Expose
     private String name;
 
-    public void setName(String name){
-        this.name=name;
+
+    @SerializedName("superior_id")
+    @Expose
+    private String superiorId;
+
+    @SerializedName("logger")
+    @Expose
+    private List<Logger> logger;
+    @SerializedName("conf")
+    @Expose
+    private Map<String, Object> conf;
+
+    public Protocol(String name, Map<String, Object> conf) {
+        this.setName(name);
+        this.setConf(conf);
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    @SerializedName("conf")
-    @Expose
-    private String conf;
-
-    public void setConf(String conf){
-        this.conf=conf;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getConf(){
+    public Map<String, Object> getConf() {
         return this.conf;
     }
 
-    public Protocol(String name,String conf){
-        this.name=name;
-        this.conf=conf;
+    public void setConf(Map<String, Object> conf) {
+        this.conf = conf;
+    }
+
+    public String getSuperiorId() {
+        return superiorId;
+    }
+
+    public void setSuperiorId(String superiorId) {
+        this.superiorId = superiorId;
+    }
+
+    public List<Logger> getLogger() {
+        return logger;
+    }
+
+    public void setLogger(List<Logger> logger) {
+        this.logger = logger;
     }
 }
