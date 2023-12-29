9875988ed67b0e9b168b0da712b2b79a4cd40039
dyrnq <dyrnq@qq.com>
2023-12-29 18:31:28 +0800

    使用SignatureAlgorithm.HS512生成Jwt的secret key

--- src/main/java/cli/Jwt.java
+++ src/main/java/cli/Jwt.java
@@ -1,9 +1,13 @@
 package cli;
 
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
 import org.noear.solon.sessionstate.jwt.JwtUtils;
 import picocli.CommandLine.Command;
 
+import java.security.Key;
 import java.util.concurrent.Callable;
 
 @Command(name = "jwt", aliases = {"j"}, description = "create jwt secret")
@@ -12,7 +16,10 @@ public class Jwt extends CommonOptions implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        String jwtSecret = JwtUtils.createKey();
+//        String jwtSecret = JwtUtils.createKey();
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String jwtSecret = (String) Encoders.BASE64.encode(key.getEncoded());
+
         //System.out.println(jwtSecret);
         System.out.println("--server.session.state.jwt.secret="+jwtSecret);
         return 0;
