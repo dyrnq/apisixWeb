76f3ba7005331ce4681e0ca9a620eab1e388caf8
dyrnq <dyrnq@qq.com>
2025-02-15 17:37:36 +0800

    增加根据id搜索route

--- src/main/java/com/dyrnq/controller/api/RouteController.java
+++ src/main/java/com/dyrnq/controller/api/RouteController.java
@@ -1,5 +1,6 @@
 package com.dyrnq.controller.api;
 
+import cn.hutool.core.util.StrUtil;
 import com.dyrnq.apisix.ApisixSDKException;
 import com.dyrnq.apisix.domain.Route;
 import com.dyrnq.apisix.response.Multi;
@@ -12,6 +13,7 @@ import org.noear.solon.core.handle.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -68,11 +70,26 @@ public class RouteController extends ApiController {
     }
 
     @Mapping("")
-    public PageResult query(Context ctx, String page, String limit, String name, String label, String uri) {
+    public PageResult query(Context ctx, String page, String limit, String id, String name, String label, String uri) {
         try {
-            Multi<Route> rsp = getAdminClient().queryRoutes(page, limit, toMap(name, label, uri));
-            List<Route> result = getAdminClient().arrangeMulti(rsp.getNodes());
-            return PageResult.succeed(result, rsp.getTotal());
+            Route route = null;
+            if (StrUtil.isNotBlank(id)) {
+                try {
+                    route = getAdminClient().getRoute(id);
+                } catch (Exception e) {
+                    logger.warn(e.getMessage(), e);
+                }
+
+            }
+            if (route == null) {
+                Multi<Route> rsp = getAdminClient().queryRoutes(page, limit, toMap(name, label, uri));
+                List<Route> result = getAdminClient().arrangeMulti(rsp.getNodes());
+                return PageResult.succeed(result, rsp.getTotal());
+            } else {
+                List<Route> result = new ArrayList<>();
+                result.add(route);
+                return PageResult.succeed(result, new Integer(1));
+            }
         } catch (ApisixSDKException e) {
             logger.error(e.getMessage(), e);
             return PageResult.failure(e.getMessage());
