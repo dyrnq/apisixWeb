c0851b954d87768ceb06bb8c641bff1ebbbaa51e
dyrnq <dyrnq@qq.com>
2025-03-01 17:46:29 +0800

    替换minio/mc的镜像

--- scripts/test-minio.sh
+++ scripts/test-minio.sh
@@ -19,14 +19,14 @@ ${minio_image} server /data --address ":19000" --console-address ":19001"
 
 sleep 5s;
 
-docker run -i --rm --network host --entrypoint '' quay.io/minio/mc bash<<EOF
+docker run -i --rm --network host --entrypoint '' minio/mc bash<<EOF
 mc alias set myminio http://localhost:19000 minioadmin minioadmin;
 mc admin user svcacct add --access-key "u5SybesIDVX9b6Pk" --secret-key "lOpH1v7kdM6H8NkPu1H2R6gLc9jcsmWM" myminio minioadmin 2>/dev/null || true;
 EOF
 
 
 
-docker run -i --rm --network host --entrypoint '' quay.io/minio/mc bash<<EOF
+docker run -i --rm --network host --entrypoint '' minio/mc bash<<EOF
 mc alias set myminio http://localhost:19000 minioadmin minioadmin;
 mc mb myminio/test 2>/dev/null || true;
 mc anonymous set download myminio/test;
