69e135c57719cb6a43a9f4a8cdc45e6aaf8327d7
dyrnq <dyrnq@qq.com>
2026-02-21 10:47:04 +0800

    代码重构

---
+++ src/main/java/com/dyrnq/Constants.java
@@ -0,0 +1,7 @@
+package com.dyrnq;
+
+public interface Constants {
+
+    String VERSION = "v1.0.0";
+    String BUILD_DATETIME ="2026-02-21 09:21:35";
+}
---
+++ src/main/java/com/dyrnq/filter/Message.java
@@ -0,0 +1,9 @@
+package com.dyrnq.filter;
+
+import lombok.Data;
+
+@Data
+public class Message {
+    private String key;
+    private String value;
+}
